52146498079cf866a660f425b48e30af497abdf4
EC2 Default User <pveijk@gmail.com>
2018-11-08 20:15:25 +0000

    step2

--- karmabot/event.py
+++ karmabot/event.py
@@ -4,8 +4,13 @@ import json
 def receive(event, context):
     data = json.loads(event['body'])
     print("Got data: {}".format(data))
+    return_body = "ok"
+
+    if data["type"] == "url_verification":
+        print("Received challenge")
+        return_body = data["challenge"]
 
     return {
         "statusCode": 200,
-        "body": "ok"
+        "body": return_body
     }
